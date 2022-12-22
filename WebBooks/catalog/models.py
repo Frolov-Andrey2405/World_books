2a0b88ed4e6fc499400f96dec04d5dbe5d1ba72a
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2022-12-22 16:00:34 +0200

    Basic elements of data models

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+class MyModelName(models.Model):
+
+    # Multiple fields
+    my_field_name = models.CharField(
+        max_length=20,
+        help_text='No more than 20 characters')
+
+    class Meta:
+        '''Metadata'''
+        ordering = ['-my_field_name']
+
+    def get_absolute_url(self):
+        '''Methods'''
+        return reverse("model-detail-view", args=[str(self.id)])
+
+    def __str__(self):
+        '''String to represent the MyModelName object in the Admin site'''
+        return self.field_name
