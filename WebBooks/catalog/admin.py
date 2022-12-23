fdc916dd7189090bf7e08782f5ee6b9c291dbe2f
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2022-12-23 16:17:59 +0200

    Forming the layout with a detailed representation of the list item

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -4,31 +4,30 @@ from .models import Author, Book, Genre, Language, Status, BookInstance
 # Register your models here.
 
 
-# --------------------------------
 @admin.register(Author)
-# --------------------------------
-
 class AuthorAdmin(admin.ModelAdmin):
     list_display = (
-        'last_name', 'first_name', 'date_of_birth', 'date_of_death'
+        'last_name', 'first_name'
     )
 
-# --------------------------------
-@admin.register(Book)
-# --------------------------------
+    fields = [
+        'first_name', 'last_name',
+        'date_of_birth', 'date_of_death'
+    ]
 
+
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = (
         'title', 'genre', 'language', 'display_author'
     )
+
     list_filter = (
         'genre', 'author'
     )
 
-# --------------------------------
-@admin.register(BookInstance)
-# --------------------------------
 
+@admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = (
         'book', 'status'
